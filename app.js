a291210eb1a9ba837a351001739bbdfb12d4ff87
www-infinity4 <ephraim.nwo@gmail.com>
2026-09-23 23:15:01 -0500

    Add verified direct MP3 classical piano recordings

--- app.js
+++ app.js
@@ -237,6 +237,9 @@ function startStream(url, fallbackSynthType) {
     'https://upload.wikimedia.org/wikipedia/commons/4/41/Mozart%2C_Wolfgang_Amadeus_%E2%80%94_Piano_Sonata_No._10_in_C_major%2C_K._330_%E2%80%94_1st_movement_%E2%80%94_Bui-Nguyen_Trieu-Tuong_%E2%80%94_MusOpen_Project.mp3',
     'https://upload.wikimedia.org/wikipedia/commons/transcoded/9/9b/Mozart_-_Piano_Sonata_No._11_in_A_major_-_II._Allegro_moderato.ogg/Mozart_-_Piano_Sonata_No._11_in_A_major_-_II._Allegro_moderato.ogg.mp3',
     'https://upload.wikimedia.org/wikipedia/commons/transcoded/3/36/Mozart_K448.ogg/Mozart_K448.ogg.mp3',
+    'https://upload.wikimedia.org/wikipedia/commons/5/50/Nocturne_Op._9_no._2_in_E_flat_major.mp3',
+    'https://upload.wikimedia.org/wikipedia/commons/5/51/PolonaiseOp.71No.2InBFlatMajor.mp3',
+    'https://upload.wikimedia.org/wikipedia/commons/a/a9/WaltzB.46InEFlatMajor.mp3',
     'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'
   ];
   let sourceIndex = 0;
